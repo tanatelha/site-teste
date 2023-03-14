8b03daeda55394b9d6bf08a4aa018715a3bcbd84
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-03-13 22:10:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ menu = """
 
 @app.route("/")
 def hello_world():
-  return "Olá, mundo! Esse é o meu site. (Natália Santos)"
+  return menu + "Olá, mundo! Esse é o meu site. (Natália Santos)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da página Sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
 
