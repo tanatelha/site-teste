d798319a1525dd60d3e80b9d9bb391260b601bb3
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-03-13 21:50:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é o meu site. (Natália Santos)"
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da página Contato"
+
