10387f1a37888ab13a65ff54481b7ed9cbdb66e7
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-03-13 22:09:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
+
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é o meu site. (Natália Santos)"
