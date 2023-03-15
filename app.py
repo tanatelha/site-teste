bbcfa060cc012860d06d74a6a9aeec3045de51f3
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-03-15 16:13:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ menu = """
 
 @app.route("/")
 def hello_world():
-  return menu + "Olá, mundo! Esse é o meu site. <br> <br> Quer saber um pouco mais sobre mim? Clique no 'Sobre' no topo da página"
+  return menu + "<br> <br> Olá, mundo! Esse é o meu site. <br> <br> Quer saber um pouco mais sobre mim? Clique no 'Sobre' no topo da página"
 
 @app.route("/sobre")
 def sobre():
-  return menu + "Você está na página Sobre! <br> <br> Se você está aqui, é porque você quer saber mais sobre mim. Para isso, vamos nos conhecer? Se quiser me conhecer, aperte em 'Contato' no topo da tela"
+  return menu + "<br> <br> Você está na página Sobre! <br> <br> Se você está aqui, é porque você quer saber mais sobre mim. Para isso, vamos nos conhecer? Se quiser me conhecer, aperte em 'Contato' no topo da tela"
 
 @app.route("/contato")
 def contato():
-  return menu + "Para entrar em contato com a desenvolvedora desse site basta fazer sinal de fumaça"
+  return menu + "<br> Para entrar em contato com a desenvolvedora desse site basta fazer sinal de fumaça"
 
