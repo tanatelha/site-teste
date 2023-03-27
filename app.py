fb3ef062d8f58f70da449a1d9e3528dd53955ede
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-03-27 20:52:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
 GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
 with open("credenciais.json", mode="w") as arquivo:
   arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
-conta = ServiceAccountCredentials.from_json("credenciais.json")
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 
 api = gspread.authorize(conta)
 planilha = api.open_by_key('1ZDyxhXlCtCjMbyKvYmMt_8jAKN5JSoZ7x3MqlnoyzAM')
